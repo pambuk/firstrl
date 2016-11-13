e73c8253aa22be27b9fa0ae64d0982ee6c90de9a
pambuk <wojtek.zymonik@gmail.com>
2016-11-13 16:55:43 +0100

    random dungeons

--- firstrl.py
+++ firstrl.py
@@ -6,6 +6,10 @@ SCREEN_HEIGHT = 50
 MAP_WIDTH = 80
 MAP_HEIGHT = 45
 
+ROOM_MAX_SIZE = 10
+ROOM_MIN_SIZE = 6
+MAX_ROOMS = 30
+
 LIMIT_FPS = 20
 
 # movement
@@ -37,14 +41,58 @@ def make_map():
     global map
 
     # fill map with unblocked tiles
-    map = [[Tile(False) 
+    map = [[Tile(True) 
             for y in range(MAP_HEIGHT)] 
                 for x in range(MAP_WIDTH)]
 
-    map[30][22].blocked = True
-    map[30][22].block_sight = True
-    map[50][22].blocked = True
-    map[50][22].block_sight = True
+    # rooms
+    rooms = []
+    nr_of_rooms = 0
+
+    for r in range(MAX_ROOMS):
+        w = libtcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+        h = libtcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+
+        x = libtcod.random_get_int(0, 0, MAP_WIDTH - w - 1)
+        y = libtcod.random_get_int(0, 0, MAP_HEIGHT - h - 1)
+
+        new_room = Rect(x, y, w, h)
+        failed = False
+        for other_room in rooms:
+            if new_room.intersect(other_room):
+                failed = True
+                break
+
+        # no intersections with other rooms found
+        if not failed:
+            create_room(new_room)
+            # room center
+            (new_x, new_y) = new_room.center()
+            room_no = Object(new_x, new_y, chr(65+nr_of_rooms), libtcod.white)
+            objects.insert(0, room_no)
+
+            if nr_of_rooms == 0:
+                player.x = new_x
+                player.y = new_y
+
+                npc.x = new_x + 1
+                npc.y = new_y + 1
+            else:
+                # @todo draw this part on paper
+                # center coordinates of previous room
+                (prev_x, prev_y) = rooms[nr_of_rooms - 1].center()
+
+                if libtcod.random_get_int(0, 0, 1) == 1:
+                    # horizontal -> vertical
+                    create_h_tunnel(prev_x, new_x, prev_y)
+                    create_v_tunnel(prev_y, new_y, new_y)
+                else:
+                    # vertical -> horizontal
+                    create_v_tunnel(prev_y, new_y, prev_x)
+                    create_h_tunnel(prev_x, new_x, new_y)
+
+            rooms.append(new_room)
+            nr_of_rooms += 1
 
 def render_all():
     global color_dark_ground
@@ -63,6 +111,30 @@ def render_all():
         object.draw()
 
     libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
+
+def create_room(room):
+    global map
+
+    for x in range(room.x1 + 1, room.x2):
+        for y in range(room.y1 + 1, room.y2):
+            map[x][y].blocked = False
+            map[x][y].block_sight = False
+
+# create horizontal tunnel
+def create_h_tunnel(x1, x2, y):
+    global map
+
+    for x in range(min(x1, x2), max(x1, x2) + 1):
+        map[x][y].blocked = False
+        map[x][y].block_sight = False
+
+# and vertical
+def create_v_tunnel(y1, y2, x):
+    global map
+
+    for y in range(min(y1, y2), max(y1, y2) + 1):
+        map[x][y].blocked = False
+        map[x][y].block_sight = False
 
 class Object:
     def __init__(self, x, y, char, color):
@@ -92,6 +164,22 @@ class Tile:
 
         self.block_sight = block_sight
 
+class Rect:
+    def __init__(self, x, y, w, h):
+        self.x1 = x
+        self.y1 = y
+        self.x2 = x + w
+        self.y2 = y + h
+
+    def center(self):
+        center_x = (self.x1 + self.x2) / 2
+        center_y = (self.y1 + self.y2) / 2
+        return (center_x, center_y)
+
+    def intersect(self, other):
+        return (self.x1 <= other.x2 and self.x2 >= other.x1 and 
+                self.y1 <= other.y2 and self.y2 >= other.y1)
+
 # init
 libtcod.console_set_custom_font("arial12x12.png", libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'python/libtcod tutorial', False)
@@ -99,7 +187,7 @@ libtcod.sys_set_fps(LIMIT_FPS)
 
 con = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
 
-player = Object(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, '@', libtcod.white)
+player = Object(25, 23, '@', libtcod.white)
 npc = Object(SCREEN_WIDTH/2 - 5, SCREEN_HEIGHT/2, '@', libtcod.yellow)
 objects = [npc, player]
 
